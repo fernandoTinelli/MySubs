d9bb7a191f98aa6c5f734de4371cabffcbb6aef8
fftinelli <fftinelli@gmail.com>
2025-07-07 08:56:15 -0300

    fix: correct input name

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -40,7 +40,7 @@ export const LoginPage = (props: Props) => {
 				<form className="space-y-4" onSubmit={handleSubmit(handleLogin)}>
 					<div>
 						<label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
-							Email
+							Username
 						</label>
 						<input
 							type="text"
