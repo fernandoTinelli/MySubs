909ab166428ca33db597ff4fd53b80b4dca15c73
fftinelli <fftinelli@gmail.com>
2025-07-07 09:28:42 -0300

    feat: add protected routes

---
+++ frontend/src/routes/ProtectedRoutes.tsx
@@ -0,0 +1,16 @@
+import { Navigate, useLocation } from "react-router-dom";
+import { useAuth } from "../context/useAuth";
+
+type Props = {
+	children: React.ReactNode;
+};
+export const ProtectedRoutes = (props: Props) => {
+	const location = useLocation();
+	const { isLoggedIn } = useAuth();
+
+	return isLoggedIn() ? (
+		<>{props.children}</>
+	) : (
+		<Navigate to="/login" state={{ from: location }} replace />
+	);
+};
