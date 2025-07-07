62471abeac2456105b7be97f9c5ae9940a121326
fftinelli <fftinelli@gmail.com>
2025-07-07 12:59:36 -0300

    feat: config backend auth api

--- frontend/src/services/AuthService.tsx
+++ frontend/src/services/AuthService.tsx
@@ -3,15 +3,19 @@ import { handleError } from "../helpers/ErrorHandler";
 import type { UserProfileToken } from "../models/User";
 
 const api = import.meta.env.VITE_API_AUTH;
+const port = import.meta.env.VITE_API_AUTH_PORT;
 const loginPath = import.meta.env.VITE_API_AUTH_LOGIN;
 const registerPath = import.meta.env.VITE_API_AUTH_REGISTER;
 
 export const loginAPI = async (username: string, password: string) => {
 	try {
-		const data = await axios.post<UserProfileToken>(`${api}${loginPath}`, {
-			username: username,
-			password: password,
-		});
+		const data = await axios.post<UserProfileToken>(
+			`${api}:${port}${loginPath}`,
+			{
+				username: username,
+				password: password,
+			}
+		);
 		return data;
 	} catch (error) {
 		handleError(error);
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -2,6 +2,7 @@
 
 interface ImportMetaEnv {
 	readonly VITE_API_AUTH: string;
+	readonly VITE_API_AUTH_PORT: string;
 	readonly VITE_API_AUTH_REGISTER: string;
 	readonly VITE_API_AUTH_LOGIN: string;
 }
