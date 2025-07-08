fc1b435d38c685f92eb9e3068be63349dfbe07a5
fftinelli <fftinelli@gmail.com>
2025-07-07 21:51:41 -0300

    fix: error handler

--- frontend/src/helpers/ErrorHandler.tsx
+++ frontend/src/helpers/ErrorHandler.tsx
@@ -15,7 +15,7 @@ export const handleError = (error: any) => {
 				toast.warning(err.data.errors[e][0]);
 			}
 		} else if (err?.data) {
-			toast.warning(err.data);
+			toast.warning(err.data.error);
 		} else if (err?.status == 401) {
 			toast.warning("Please login");
 			window.history.pushState({}, "LoginPage", "/login");
