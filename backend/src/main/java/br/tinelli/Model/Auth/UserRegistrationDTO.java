9f4e72e2d413df8c4822d7cfb8ac18b4aee7d87c
fftinelli <fftinelli@gmail.com>
2025-07-07 23:20:25 -0300

    fix: user registration

--- backend/src/main/java/br/tinelli/Model/Auth/UserRegistrationDTO.java
+++ backend/src/main/java/br/tinelli/Model/Auth/UserRegistrationDTO.java
@@ -1,12 +1,13 @@
 package br.tinelli.Model.Auth;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record UserRegistrationDTO(
-    @Email String email,
-    @NotBlank String userName,
-    @Min(10) @Max(100) String password
+    @Email(message = "email invalid") String email,
+    @NotBlank(message = "username required") String username,
+    
+    @Size(min = 10, max = 100, message = "password must have between 10 and 100 characters")
+    String password
 ) {}
